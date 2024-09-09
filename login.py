4b7e2d4eb0c6142bf9f082cbdf4eaef6a0bbdcc3
ABHAY CHOUDHARY <156689021+abhaychoudhary11@users.noreply.github.com>
2024-09-09 21:19:00 +0530

    Update login.py

--- login.py
+++ login.py
@@ -9,39 +9,38 @@ home.resizable(True,True)
 
 
 #backgroundimage
-img=Image.open("bglogin.jpg")
+img=Image.open("bgbank.png")
 img=img.resize((1566,790))
 bg= ImageTk.PhotoImage(img)
 Label(home,image=bg,bg='white').place(x=0,y=0)
 
-#banklogo
-logo1=Image.open("banklogo.png")
-logo1=logo1.resize((500,500))
-banklogo= ImageTk.PhotoImage(logo1)
-Label(home,image=banklogo).place(x=950,y=130)   
 
 #frame
-frame=Frame(home,width=600,height=600,bg="white")
-frame.place(x=100,y=80)
-heading=Label(frame,text='Login',fg='#57a1f8',bg='white',font=('Poppins',19,'bold'))
+frame=Frame(home,width=600,height=400,bg="#095e7d")
+frame.place(x=100,y=120)
+heading=Label(frame,text='Login',fg='white',bg='#095e7d',font=('Poppins',19,'bold'))
 heading.place(x=250,y=5)
+
 
 #id password
 #ID
-Label(frame,text='User Id',fg='black',bg='white',font=('Poppins',17,'bold')).place(x=80,y=80)
+ulogo= PhotoImage(file='user1.png')
+Label(frame,image=ulogo,width=50,height=50,bg='#095e7d',fg='white').place(x=38,y=59)
+Label(frame,text='User Id',fg='white',bg='#095e7d',font=('Poppins',14)).place(x=70,y=90)
 user= Entry(frame,width=65,fg='black',border=2,bg="white",font=('Poppins',11))
 user.place(x=30,y=120)
-#uidlogo= ImageTk.PhotoImage("usernid.png")
-#Label(frame,image=uidlogo).place()
+
 
     
 #PASSWORD   
-Label(frame,text='Password',fg='black',bg='white',font=('Poppins',17,'bold')).place(x=80,y=150)
-password= Entry(frame,width=65,fg='black',border=2,bg="white",font=('Poppins',11))
-password.place(x=30,y=190)
+passwordlogo= PhotoImage(file='password1.png')
+Label(frame,image=passwordlogo,width=50,height=50,bg='#095e7d',fg='white').place(x=39,y=159)
+Label(frame,text='Password',fg='white',bg='#095e7d',font=('Poppins')).place(x=70,y=190)
+password= Entry(frame,width=65,show='*',fg='white',border=2,bg="white",font=('Poppins',11))
+password.place(x=30,y=220)
 
 #button
-Button(frame,width=39,pady=7,text='Login',bg='#57a1f8',fg='white',border=0).place(x=160,y=240)
+Button(frame,width=20,pady=7,text='Login',bg='#57a1f8',fg='white',border=0).place(x=210,y=280)
 
 
 
